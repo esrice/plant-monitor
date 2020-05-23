20b245c08e14bf891d41930dda98fcdabcf0ce2d
= <edward.rice@missouri.edu>
2020-05-23 02:59:45 +0100

    Added code for communicating with the atmega
    Started a python package and module for the pi to be able to communicate
    with the atmega over SPI

---
+++ addie/serial.py
@@ -0,0 +1,58 @@
+import spidev
+
+commands = {
+    'checksum': 1,
+    'temp_int': 2,
+    'temp_dec': 3,
+    'hum_int': 4,
+    'hum_dec': 5,
+}
+
+
+class BadChecksumError(Exception):
+    pass
+
+
+class Monitor:
+    def __init__(self, bus=0, device=0, speed=5000):
+        self.spi = spidev.SpiDev()
+        self.spi.open(bus, device)
+        self.spi.max_speed_hz = speed
+
+    def read_all(self):
+        """
+        Read everything from the monitor there is to read. For now,
+        that's just temperature and humidity. Check the checksum to
+        make sure the transmission is good, and raise a
+        BadChecksumError if that check fails.
+
+        Returns:
+            temp_c: temperature in degrees C
+            humidity: relative humidity in %
+        """
+        # send a series of commands and receive the results. Add an
+        # arbitrary command at the end because the result for a command
+        # comes with the next transfer
+        results = self.spi.xfer([
+            commands['checksum'],
+            commands['temp_int'],
+            commands['temp_dec'],
+            commands['hum_int'],
+            commands['hum_dec'],
+            0
+        ])
+
+        checksum = results[1]
+        temp_int, temp_dec = results[2:4]
+        hum_int, hum_dec = results[4:6]
+
+        if checksum != ((temp_int + temp_dec + hum_int + hum_dec) & 0xFF):
+            raise(BadChecksumError())
+
+        temp_c = (((temp_int & 0x7F) << 8) | temp_dec) * 0.1
+        if temp_int & 0x80:
+            temp *= -1
+        humidity = ((hum_int << 8) | hum_dec) * 0.1
+
+        return temp_c, humidity
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='addie',
+    version='0.0.0',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'spidev',
+    ],
+)
