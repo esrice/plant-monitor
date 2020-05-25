8311cde61b3fdac194e407a0f702587fef956acc
= <edward.rice@missouri.edu>
2020-05-25 15:15:37 -0700

    Got flask server running

--- addie/views.py
+++ addie/views.py
@@ -2,16 +2,26 @@ import sqlite3
 
 import flask
 
+from addie import app
+
+
+def get_config():
+    with app.open_resource('config.json', 'r') as config_file:
+        return flask.json.load(config_file)
+
+
 @app.route('/')
 def index():
     return flask.render_template('index.html')
 
 
-@app.route('/get_data'):
+@app.route('/get_data')
+def get_data():
     """
     Get a complete table dump in json.
     """
-    conn = sqlite3.connect('addie.db')
+    config = get_config()
+    conn = sqlite3.connect(config['db'])
     c = conn.cursor()
     c.execute('PRAGMA table_info(dht_readings)')
     colnames = [r[1] for r in c.fetchall()]
---
+++ run_flask_debug.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=addie
+export FLASK_ENV=development
+flask run --host=0.0.0.0
